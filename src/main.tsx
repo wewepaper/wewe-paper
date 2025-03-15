e6c34725435afabc076811638d001195d942f09f
Hye-In Yoon <heain1001@naver.com>
2025-03-15 22:03:28 +0900

    ADD : Firebase 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { app } from "./firebase.ts";
+
+console.log("Firebase Initialized:", app);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
